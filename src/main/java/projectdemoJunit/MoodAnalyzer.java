a64cdc29adec08c5abae2f038f3179cc2809fab1
Harshal1031 <harshalmalviya@gmail.com>
2023-10-10 11:46:31 +0530

    Refactor UC1

--- src/main/java/projectdemoJunit/MoodAnalyzer.java
+++ src/main/java/projectdemoJunit/MoodAnalyzer.java
@@ -1,7 +1,13 @@
 package projectdemoJunit;
 
 public class MoodAnalyzer {
-
+	String msg;
+	public MoodAnalyzer() {
+		
+	}
+  public MoodAnalyzer(String msg) {
+	  this.msg=msg;
+ }
 	public Object moodForSad(String str) {
 		String str2="I am in Sad Mood";
 		if(str.equals(str2))
@@ -13,6 +19,12 @@ public class MoodAnalyzer {
 	public Object moodForAny(String string) {
 		if(string.equals("I am in Any Mood"))
 			return "HAPPY";
+		else
+			return null;
+	}
+	public Object testMoodForSadwithConstructor() {
+		if(this.msg.equals("I am in Sad Mood")||this.msg.equals("I am in Happy Mood"))
+		return "SAD";
 		else
 			return null;
 	}
--- src/test/java/projectdemoJunit/TestMoodAnaluzer.java
+++ src/test/java/projectdemoJunit/TestMoodAnaluzer.java
@@ -17,6 +17,19 @@ public class TestMoodAnaluzer {
 	      assertEquals("HAPPY" ,mood.moodForAny("I am in Any Mood"));
 
 	}
+	@Test
+	public void testMoodForSadUsingConstructor() {
+	     MoodAnalyzer mood2=new MoodAnalyzer("I am in Sad Mood");
+	     assertEquals("SAD",mood2.testMoodForSadwithConstructor());
+
+	}
+	@Test
+	public void testMoodForHappyUsingConstructor() {
+	     MoodAnalyzer mood2=new MoodAnalyzer("I am in Happy Mood");
+	     assertEquals("SAD",mood2.testMoodForSadwithConstructor());
+
+	}
+
 	
 	
 	
